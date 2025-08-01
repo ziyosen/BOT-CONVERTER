ef4e2f32ff321d5328dcaec6cad053468fef8337
ziyosen <lozystore@outlook.co.id>
2025-08-02 04:15:23 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,8 +2,8 @@ import { generateClashConfig, generateNekoboxConfig, generateSingboxConfig } fro
 
 export default class TelegramBot {
   constructor(token, apiUrl = 'https://api.telegram.org') {
-    this.token = 7616969823:AAFglRaOaVE0ek0u0QbYXYgI6JY8IUN4M6E;
-    this.apiUrl = https://api.telegram.org/bot7616969823:AAFglRaOaVE0ek0u0QbYXYgI6JY8IUN4M6E;
+    this.token = '7616969823:AAFglRaOaVE0ek0u0QbYXYgI6JY8IUN4M6E';
+    this.apiUrl = 'https://api.telegram.org/bot7616969823:AAFglRaOaVE0ek0u0QbYXYgI6JY8IUN4M6E';
   }
 
   async handleUpdate(update) {
